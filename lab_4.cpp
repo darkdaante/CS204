0f7273d7a9064dab61bda280910518c7b8097ec0
darkdaante <45766636+darkdaante@users.noreply.github.com>
2019-08-26 00:27:03 +0530

    Update lab_4.cpp

--- lab_4.cpp
+++ lab_4.cpp
@@ -1,208 +1,232 @@
 #include <bits/stdc++.h>
-#define f(a, b, c) for (long long int a = b; a < c; a++)
+
 using namespace std;
 
+struct tree 
+{                                
+    string data; 
+    tree* left, *right; 
+}; 
 
-int ord(string a) 
+tree* newNode(string v) 
+{                                
+    tree *temp = new tree; 
+    temp->left = temp->right = NULL; 
+    temp->data = v;
+
+    return temp; 
+}; 
+
+vector<string> formvector(string s)
+{
+	int n = s.size();
+	vector<string> st;
+	for(int i = 0; i<n;i++)
+	        { string temp="";
+	         if(s[i]=='-'&&(i==0))
+	           {	             temp=+s[0];
+	                             int j = i+1;
+	                             for(;(s[j] >= '0' && s[j] <= '9');j++) 
+	                              {
+	                             temp+=s[j];
+	                               }
+	               		       i=j-1;
+	           }
+	     else{           
+	                      if((s[i] >= '0' && s[i] <= '9')) 
+        	                 { int j = i;
+	                                for(;(s[j] >= '0' && s[j] <= '9');j++)
+                                            {
+	                                     temp+=s[j];
+                                            }
+	                               i = j-1;
+	                          }
+	                      else 
+	                         { 
+                                      if(s[i]=='-'&&!(s[i-1] >= '0' && s[i-1] <= '9'))
+                                       {   temp=+s[i];
+                                            int j = i+1;
+	                                        for(;(s[j] >= '0' && s[j] <= '9');j++) 
+	                                        {
+	                                         temp+=s[j];
+	                                        }
+                                           i=j-1;
+                                      }
+                                      else {temp+=s[i];} 
+	                         }
+	        
+	         }   st.push_back(temp);
+	      }
+	        return st;
+
+	}
+
+int prec(char c)   
 { 
-    if(a == "^") 
+    if(c == '^') 
     return 3; 
-    else if(a == "*" || a == "/") 
+    else if(c == '*' || c == '/') 
     return 2; 
-    else if(a == "+" || a == "-") 
+    else if(c == '+' || c == '-') 
     return 1; 
     else
     return -1; 
 } 
 
-vector<string> sTv(string s)
-{
-    int n = s.length();
-    vector<string> stv;
-    for(int i = 0; i<n;i++)
-    {
-        string temp="";
-        if((s[i] >= '0' && s[i] <= '9')) 
-        {
-            int j = i;
-            for(;(s[j] >= '0' && s[j] <= '9');j++)
-            {
-                temp+=s[j];
-            }
-            i = j-1;
-        }
-        else 
-        {
-            temp+=s[i];
-        }
-        stv.push_back(temp);
-
-    }
-
-    return stv;
-}
-
-
-vector<string> inftopos(vector<string> s) 
+int convert(string s)   
+{  
+    int num = 0;
+                                      
+    if(s[0]!='-') 
+        for (int i=0; i<s.length(); i++)  
+            num = num*10 + (int(s[i])-48);  
+                                            
+    else{
+        for (int i=1; i<s.length(); i++)  
+        { 
+            num = num*10 + (int(s[i])-48);  
+            
+        } 
+       num = num*-1;}
+  return num;  
+}  
+                              
+   //for infix to postfix 
+vector<string> conversion(vector<string> s) 
 { 
     stack<string> st; 
-    st.push("N"); 
-    int l = s.size(); 
-    vector<string> ns;
+     int l = s.size(); 
+    vector<string> ns; 
     for(int i = 0; i < l; i++) 
     { 
-        if((s[i][0] >= '0' && s[i][0] <= '9')) 
-            ns.push_back(s[i]);
-        else if(s[i] == "(") 
-           st.push("("); 
-        else if(s[i] == ")") 
-        { 
-            while(st.top() != "N" && st.top() != "(") 
-            { 
-                string c = st.top(); 
-                st.pop(); 
-                ns.push_back(c);
-            } 
-            if(st.top() == "(") 
-            { 
-                string c = st.top(); 
-                st.pop(); 
-            } 
-        } 
-        else{ 
-            while(st.top() != "N" && ord(s[i]) <= ord(st.top())) 
-            { 
-                if(s[i]=="^"&&st.top()=="^") break;
-                string c = st.top(); 
-                st.pop(); 
-                ns.push_back(c); 
-            } 
-            st.push(s[i]); 
-        } 
-  
-    } 
-    while(st.top() != "N") 
+                            
+         if(!(((s[i][0]=='*')||(s[i][0]=='+')||s[i][0]=='-'||(s[i][0]=='/')||(s[i][0]=='^'))&&(s[i].size()==1)))
+          {  
+              ns.push_back(s[i]);
+          }
+         else             //for operation
+          { if(st.empty()) { st.push(s[i]);}        //if stack is empty then push directly
+            else{      if(prec(s[i][0])>prec((st.top())[0])) { st.push(s[i]); }
+                       else{        if(prec(s[i][0])==prec((st.top())[0]))
+                                      {  if((s[i][0]!='^')) { ns.push_back(st.top());
+                                                              st.pop();
+                                                              st.push(s[i]);}  
+                                         else{ st.push(s[i]); }
+                                      }
+
+                                    else {if(prec(s[i][0])<prec((st.top())[0]))
+                                              { while((!st.empty()))//check if work with empty
+                                                  { ns.push_back(st.top());
+                                                    st.pop(); 
+                                                  }
+                                                   st.push(s[i]); 
+                                               }
+                                          }
+                           }
+                }
+          }
+    }
+    while(!st.empty()) 
     { 
         string c = st.top(); 
         st.pop(); 
-        ns.push_back(c); 
+        ns.push_back(c);
     } 
-      
-    return ns;
-  
-} 
-
-struct node
-{
-  string s;
-  node* left=NULL,*right=NULL;
-};
-
-node* ct(vector <string> PFIX)
-{
-       node *root=NULL;
-       vector <node*> stack1;
-       int i=0;
-       while(i!=PFIX.size())
-       {
-         if(PFIX[i]!="+" && PFIX[i]!="-" && PFIX[i]!="*" && PFIX[i]!="/" && PFIX[i]!="^")
-           {
-                 node *temp=(node *)malloc(sizeof(node));
-
-                 temp->s=PFIX[i];
-
-                 f(j,0,PFIX[i].size())
-
-                 if(PFIX[i][j]<'0' || PFIX[i][j]>'9')return NULL;
-
-                 stack1.push_back(temp);
-           } 
-         else
-         {
-               if(stack1.size()<2)return NULL;
-
-               node *s1=stack1.back();
-
-               stack1.pop_back();
-
-               node *s2=stack1.back();
-
-               stack1.pop_back();
-
-               node *temp=(node *)malloc(sizeof(node));
-
-               temp->s=PFIX[i];
-
-               temp->left=s2;
-
-               temp->right=s1;
-
-               stack1.push_back(temp); 
-         }
-         i++;
-       }
-       if(stack1.size()!=1)
-            return NULL;
-       return stack1.back();
-}
+return ns;
+}       
 
 
-long long int evaluate(node *root)
-{
-   string s=root->s;
-   long long int answer=0;
-   if(s!="+" && s!="-" && s!="*" && s!="/" && s!="^")
-   return stoi(s);
-   else
-   {
-     int lanswer=evaluate(root->left),ranswer=evaluate(root->right);
-
-     if(s=="+")
-       answer=lanswer+ranswer;
-     else if(s=="-")
-       answer=lanswer-ranswer;
-     else if(s=="*")
-       answer=lanswer*ranswer;
-     else if(s=="/")
-       answer=lanswer/ranswer;
-     else
-     {
-       answer=1;
-       f(i,0,ranswer)answer*=lanswer;
-     }
-   }
-   return answer;
-}
-int main() 
+tree* buildtree(vector<string> postfix) 
 { 
-  int q;
-  cin>>q;
-  while(q--)
-  {
-    int n;
-    cin>>n;
-    for(long long int i = 0;i<n;i++)
-    {
-      string s;
+    stack<tree*> st; 
+    tree *t, *t1, *t2; 
+                            
+    for (int i=0; i<postfix.size(); i++) 
+    { 
+        
+         if(!(((postfix[i][0]=='*')||(postfix[i][0]=='+')||postfix[i][0]=='-'||(postfix[i][0]=='/')||(postfix[i][0]=='^'))&&(postfix[i].size()==1)))
+        { 
+            t = newNode(postfix[i]); 
+            st.push(t); 
+        } 
+        else  
+        { 
+            t = newNode(postfix[i]); 
+  
+            // Pop two top nodes 
+            t1 = st.top();  
+            st.pop();       
+            t2 = st.top(); 
+            st.pop(); 
+           
+            t->right = t1; 
+            t->left = t2; 
+  
+              st.push(t); 
+        } 
+    } 
+  
+    t = st.top(); 
+    st.pop(); 
+  
+    return t; 
+} 
+  
 
-      cin>>s;
 
-      vector <string> newv=sTv(s);
 
-      newv=inftopos(newv);
+int eval(tree* root)  
+{  
+    // empty tree  
+    if (!root)  
+        return 0;  
+  
+    // leaf node i.e, an integer  
+    if (!root->left && !root->right) return convert(root->data);
+  
+    // Evaluate left subtree  
+    long int l_val = eval(root->left);
+    
+  
+    // Evaluate right subtree  
+    long int r_val = eval(root->right);
 
-      node* root=ct(newv);
 
-      if(root!=NULL)
-        {
-          cout<<evaluate(root)<<"\n";
-        }
-      else 
-      {
-        cout<<"CANT BE EVALUATED\n";
+    // Check which operator to apply  
+    if (root->data=="+")  
+        return l_val+r_val;  
+  
+    if (root->data=="-")  
+        return l_val-r_val;  
+  
+    if (root->data=="*")  
+        return l_val*r_val;  
+    if (root->data=="/")
+       return l_val/r_val;
+    if (root->data=="^")
+       return pow(l_val,r_val);
+    
+}  
+  
+int main()
+{ 
+int t,n;
+  cin>>t;        
+  cin>>n;      
+  for(int i=0; i<t;i++)
+  {   
+      for(int j=0;j<n;j++)
+      {  string s;
+          cin>>s;
+          vector<string> postfix=formvector(s);
+          postfix=conversion(postfix);    
+          tree* root=buildtree(postfix);
+          if(root!=NULL)
+          {
+          cout<<eval(root)<<"\n";
+          }
+          else{cout<<"CAN'T BE EVALUATED\n";}
       }
-    }
   }
-  return 0; 	
+return 0;
 }
-
